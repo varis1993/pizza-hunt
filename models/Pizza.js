377fe88ffd5bc7fb8147d7f152a8c9cfee5f6ca4
Tavaris Thompson <varis93@yahoo.com>
2022-02-21 21:18:59 -0500

    fixed mongo

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const Pizza = model('Pizza', PizzaSchema);
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -18,5 +17,7 @@ const PizzaSchema = new Schema({
     },
     toppings: []
   });
+
+  const Pizza = model('Pizza', PizzaSchema);
 
 module.exports = Pizza;
